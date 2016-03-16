b646629b72ab1076f1b9aa6c499acb45e49f174a
Kasper Røgen <kasperhangard@hotmail.com>
2016-03-16 12:31:51 +0100

    Initial Game Foundation

---
+++ Assets/MouseManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseManager : MonoBehaviour {
+
+    Vector2 mousePosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(GetClickedEntity(mousePosition));
+        }
+	}
+
+
+    void LateUpdate ()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.DrawRay(mousePosition, Vector2.up * 0.5f, Color.red);
+        Debug.DrawRay(mousePosition, Vector2.down * 0.5f, Color.red);
+        Debug.DrawRay(mousePosition, Vector2.right * 0.5f, Color.red);
+        Debug.DrawRay(mousePosition, Vector2.left * 0.5f, Color.red);
+    }
+
+
+
+    string GetClickedEntity(Vector2 mousePosition)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(mousePosition, Camera.main.transform.forward);
+        if (hit)
+        {
+            if(hit.collider.name == "Cat")
+                hit.transform.GetComponent<PositionRandomizer>().RandomizePosition();
+            if (hit.collider.name == "Cat_Level")
+                Application.LoadLevel(1);
+            return hit.collider.name;
+        }
+        else
+        {
+            return "-1";
+        }
+    }
+}
---
+++ Assets/PositionRandomizer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionRandomizer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void RandomizePosition() {
+        Vector2 tempVect = transform.position;
+        tempVect.x = Random.Range(0, 3);
+        tempVect.y = Random.Range(0, 3);
+        transform.position = tempVect;
+    }
+}
