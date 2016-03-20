005225d50799732715efd2d37badc0539d9f231b
Kasper Røgen <kasperhangard@hotmail.com>
2016-03-20 16:32:57 +0100

    Campfire Minigame Upgrade mousemanager to detect GUI too

---
+++ Assets/Scripts/CampfireGame.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CampfireGame : MonoBehaviour {
+    string CurrentResource = null;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            CurrentResource = GetComponent<MouseManager>().GetClickedEntity(mousePosition) 
+                != null ? GetComponent<MouseManager>().GetClickedEntity(mousePosition).GetComponent<Text>().text : null;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log(CurrentResource);
+            if (GetComponent<MouseManager>().GetClickedEntity(mousePosition))
+            GetComponent<MouseManager>().GetClickedEntity(mousePosition).GetComponent<Text>().text = CurrentResource;
+        }
+
+	}
+}
--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -6,21 +6,21 @@ public class MouseManager : MonoBehaviour {
 
     Vector2 mousePosition;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log(GetClickedEntity(mousePosition));
         }
-	}
+    }
 
 
-    void LateUpdate ()
+    void LateUpdate()
     {
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Debug.DrawRay(mousePosition, Vector2.up * 0.5f, Color.red);
@@ -31,9 +31,14 @@ public class MouseManager : MonoBehaviour {
 
 
 
-    GameObject GetClickedEntity(Vector2 mousePosition)
+    public GameObject GetClickedEntity(Vector2 mousePosition)
     {
         RaycastHit2D hit = Physics2D.Raycast(mousePosition, Camera.main.transform.forward);
+        if (hit)
+        {
+            return hit.collider.gameObject;
+        }
+        hit = Physics2D.Raycast(Input.mousePosition, Camera.main.transform.forward);
         if (hit)
         {
             return hit.collider.gameObject;
