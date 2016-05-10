68084c4416fd90ed7afa47feb99be5aa6186030b
Kasper Andersen <Kasper Andersen>
2016-05-10 15:09:05 +0200

    updated the waterdistill game and the journal entry

--- Assets/Scripts/JournalEntry.cs
+++ Assets/Scripts/JournalEntry.cs
@@ -1,22 +1,8 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
 class JournalEntry : JournalEntryBase 
 {	
-	// Constructor calls base Constructor
-	public JournalEntry (string headline) : base (headline) {}
+	public JournalEntry (string headline) : base (headline) { }
 
-	// Constructor calls base Constructor
-	public JournalEntry (string headline, string body) : base (headline, body) {}
-
-	public override List<JournalEntryBase> GetEntries ()
-	{
-		throw new System.NotImplementedException();
-	}
-
-	public override void Select (int selection)
-	{
-		throw new System.NotImplementedException();
-	}
+	public JournalEntry (string headline, string body) : base (headline, body) { }
 }
