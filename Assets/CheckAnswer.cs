085052903926baf9231e903bce257072c9265eac
Kasper Røgen <kasperhangard@hotmail.com>
2016-05-12 14:40:59 +0200

    Third campfire minigame, foundation and concept

---
+++ Assets/CheckAnswer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CheckAnswer : MonoBehaviour {
+
+    GameObject parent;
+    GameObject questions;
+	// Use this for initialization
+	void Start () {
+        parent = this.gameObject;
+        questions = GameObject.Find("Questions");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void CheckAnswers(int questionNumber) {
+        switch (questionNumber) {
+            case 0:
+                Transform canvas = questions.transform.GetChild(questionNumber);
+                Debug.Log(canvas.name);
+                if (canvas.GetChild(0).GetComponent<InputField>().text == "Kinetic" ||
+                    canvas.GetChild(0).GetComponent<InputField>().text == "kinetic") { 
+                    parent.GetComponent<MiniGameCampfireFriction>().smoke.GetComponent<Image>().fillAmount += 0.1f;
+                    parent.GetComponent<MiniGameCampfireFriction>().speed += 1;
+                    canvas.gameObject.SetActive(false);
+                    Debug.Log("Question " + (questionNumber+1).ToString());
+                    questions.transform.GetChild(questionNumber + 1).gameObject.SetActive(true);
+                }
+                break;
+        }
+    }
+
+}
---
+++ Assets/MiniGameCampfireFriction.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MiniGameCampfireFriction : MonoBehaviour {
+    public GameObject stick;
+    public Transform target;
+    public Transform target2;
+    public Image smoke; 
+    public float speed;
+    bool atBottom = false;
+
+	void Update () {
+        float step = speed * Time.deltaTime;
+
+        if (Vector3.Distance(target.position, stick.transform.position) < 0.1f)
+            atBottom = true;
+        else if (Vector3.Distance(target2.position, stick.transform.position) < 0.1f)
+            atBottom = false;
+        if(!atBottom)
+            stick.transform.position = Vector3.MoveTowards(stick.transform.position, target.position, step);
+        else
+            stick.transform.position = Vector3.MoveTowards(stick.transform.position, target2.position, step);
+    }
+}
