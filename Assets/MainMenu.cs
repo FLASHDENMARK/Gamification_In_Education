020b4d60eb40d228016880602278b6b350f6ca5d
Kasper Røgen <kasperhangard@hotmail.com>
2016-05-13 09:06:39 +0200

    Quit game added to main menu Fixed unlocks and shadows offset

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -22,10 +22,12 @@ public class MainMenu : MonoBehaviour {
             GUI.Box(new Rect(0, 0, 20000, 10000), "");
 
             GUI.skin.button.fontSize = (int)(Screen.height * 0.05f);
-            if (GUI.Button(new Rect(Screen.width / 2 - Screen.width / 12, Screen.height / 1.5f, Screen.width / 6, Screen.height / 10), "Main Menu"))
+            if (GUI.Button(new Rect(Screen.width / 2 - Screen.width / 12, Screen.height / 3, Screen.width / 6, Screen.height / 10), "Main Menu"))
                 SceneManager.LoadScene(0);
-            if (GUI.Button(new Rect(Screen.width / 2 - Screen.width / 12, Screen.height / 2, Screen.width / 6, Screen.height / 10), "Survey"))
+            if (GUI.Button(new Rect(Screen.width / 2 - Screen.width / 12, Screen.height / 6, Screen.width / 6, Screen.height / 10), "Survey"))
                 Application.OpenURL("https://da.surveymonkey.com/r/6NK3XYZ");
+            if (GUI.Button(new Rect(Screen.width / 2 - Screen.width / 12, Screen.height / 1.5f, Screen.width / 6, Screen.height / 10), "Exit Game"))
+                Application.Quit();
             GUI.skin.button.fontSize = oldFontSize;
         }
     }
