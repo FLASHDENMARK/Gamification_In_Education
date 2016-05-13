cab4dcd81ef9380fa8c40e9951e18d222874eead
Kasper Røgen <kasperhangard@hotmail.com>
2016-05-13 13:18:53 +0200

    Third campfire minigame "Friction" done.

--- Assets/CheckAnswer.cs
+++ Assets/CheckAnswer.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class CheckAnswer : MonoBehaviour {
@@ -17,18 +17,39 @@ public class CheckAnswer : MonoBehaviour {
 	
 	}
 
+    public void complete() {
+        GameObject.Find("GameManager").GetComponent<ProgressionManager>().unlocks[0].level++;
+        SceneManager.LoadScene(0);
+    }
+
+    void progress(int questionNumber) {
+        Transform canvas = questions.transform.GetChild(questionNumber);
+        parent.GetComponent<MiniGameCampfireFriction>().smoke.GetComponent<Image>().fillAmount += 0.333f;
+        parent.GetComponent<MiniGameCampfireFriction>().speed += 3.333f;
+        canvas.gameObject.SetActive(false);
+        questions.transform.GetChild(questionNumber + 1).gameObject.SetActive(true);
+    }
+
     public void CheckAnswers(int questionNumber) {
+        Transform canvas = questions.transform.GetChild(questionNumber);
         switch (questionNumber) {
             case 0:
-                Transform canvas = questions.transform.GetChild(questionNumber);
-                Debug.Log(canvas.name);
                 if (canvas.GetChild(0).GetComponent<InputField>().text == "Kinetic" ||
-                    canvas.GetChild(0).GetComponent<InputField>().text == "kinetic") { 
-                    parent.GetComponent<MiniGameCampfireFriction>().smoke.GetComponent<Image>().fillAmount += 0.1f;
-                    parent.GetComponent<MiniGameCampfireFriction>().speed += 1;
-                    canvas.gameObject.SetActive(false);
-                    Debug.Log("Question " + (questionNumber+1).ToString());
-                    questions.transform.GetChild(questionNumber + 1).gameObject.SetActive(true);
+                    canvas.GetChild(0).GetComponent<InputField>().text == "kinetic") {
+                    progress(questionNumber);
+                }
+                break;
+
+            case 1:
+                if (canvas.GetChild(0).GetComponent<InputField>().text == "34,5" ||
+                    canvas.GetChild(0).GetComponent<InputField>().text == "34.5") {
+                    progress(questionNumber);
+                }
+                break;
+            case 2:
+                if (canvas.GetChild(0).GetComponent<InputField>().text == "300" ||
+                    canvas.GetChild(0).GetComponent<InputField>().text == "300.0"){
+                    progress(questionNumber);
                 }
                 break;
         }
